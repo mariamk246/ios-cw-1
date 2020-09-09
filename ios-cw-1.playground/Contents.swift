1f6d74cdd7c323309ee232474046821f0536e41e
mariam <mariooom456@gmail.com>
2020-09-09 20:21:37 +0300

    cw done

---
+++ ios-cw-1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str = "Hello, playground"
+let  name =  " Mariam"
+var hobby1 = "cooking "
+var hobby2 = " swimming "
+var age = 18
+var height = 171
+print ("hi my name is \(name) My age is \(age) my height is \(height) my hobby is \(hobby1) ")
+
+let name2 = " lena"
+var age2 =  16
+var height2 = 169
+var hobby12 = " coding"
+var hobby22 = " swimming "
+
+var agediffrence = age - age2
+print (agediffrence)
+
+hobby2 == hobby22
+age == age2
